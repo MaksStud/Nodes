41271edc9dbf7772245319319861279ea39dd31e
MaksStud <119805355+MaksStud@users.noreply.github.com>
2023-11-24 12:17:42 +0200

    added the transferring_data method to transfer data from a copy to the main one

--- data_node.py
+++ data_node.py
@@ -13,13 +13,23 @@ class DataNode:
         self.app.add_url_rule('/receive', 'get_data', self.get_data)
         self.app.add_url_rule('/send', 'set_data', self.set_data, methods=['POST'])
         self.app.add_url_rule('/stats', 'workload', self.workload)
-        self.app.add_url_rule("/send_copy", 'set_copy_data', self.set_copy_data, methods=['POST'])
+        self.app.add_url_rule('/send_copy', 'set_copy_data', self.set_copy_data, methods=['POST'])
+        self.app.add_url_rule('/transfer', 'transferring_data', self.transferring_data)
 
     def set_data(self):
         new_data = request.data.decode('utf-8')
         self.data.put(new_data)
         print(f"add new message = {new_data}")
         return make_response()
+
+    def transferring_data(self):
+        if self.data.qsize() > 0:
+            while not self.copy_data.empty():
+                element = self.copy_data.get()
+                self.data.put(element)
+                return 'True'
+        else:
+            return 'False'
 
     def set_copy_data(self):
         new_copy = request.data.decode("utf-8")
